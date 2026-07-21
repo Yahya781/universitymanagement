416ea9cca96195f7d0d91583b7e2782d9011bbf0
yahya781 <yahyaparekh20@gmail.com>
2026-07-21 16:06:31 +0500

    UI build for student

---
+++ WebApplication1/Views/student/studentcontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace universitymanagementsystem.Views.students
+{
+    public class student : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
